63ec64246654c216bf194a4ff9a29fec7ce0a2ee
gcli <star106121@gmail.com>
2019-03-21 20:37:50 +0800

    Correctly get the information from /proc/net/tcp, tcp6, udp, udp6.

--- lab1/handle-string.hpp
+++ lab1/handle-string.hpp
@@ -1,22 +1,40 @@
 #include <cstring>
 
-#define LEN 256
+#define N 99
+#define LEN 999
 
 const char *proc_path = "/proc";
 const char *tcp_file_path = "/proc/net/tcp";
+const char *tcp6_file_path = "/proc/net/tcp6";
 const char *udp_file_path = "/proc/net/udp";
+const char *udp6_file_path = "/proc/net/udp6";
 
-unsigned short is_contain_letter(const char *s) {
-	int l = strlen(s);
-	for (int i = 0; i < l; i++)
-		if (s[i] < '0' || s[i] > '9')
-			return 1;
-	return 0;
+typedef struct stat {
+	char protocol[N];
+	char local_address[N];
+	char foreign_address[N];
+	char pid[N];
+	char program[LEN];
+} Stat;
+
+bool is_dot(const char *name) {
+    if (!strcmp(name, ".") || !strcmp(name, ".."))
+        return true;
+    return false;
+}
+
+bool is_not_pid(const char *name) {
+    int l = strlen(name);
+    for (int i = 0; i < l; i++)
+        if (name[i] < '0' || name[i] > '9')
+            return true;
+
+    return false;
 }
 
 void cat_path(char *ret, const char *a, const char *b) {
 	int l = strlen(a);
-	memset(ret, 0, sizeof(ret));
+	memset(ret, 0, LEN);
 	strcat(ret, a);
 	if (a[l-1] == '/' && b[0] == '/')
 		ret[l-1] = '\0';
--- lab1/main.cpp
+++ lab1/main.cpp
@@ -1,78 +1,153 @@
 #include <cstdio>
 #include <cstdlib>
 #include <dirent.h>
+#include <unistd.h>
+#include <vector>
+#include <map>
 #include "handle-string.hpp"
 
 extern const char *proc_path;
 extern const char *tcp_file_path;
 extern const char *udp_file_path;
 
-void traverse_pid(const char *pid_path) {
-	DIR *dir;
-	struct dirent *f;
-	char tmp[LEN] = {0};
-
-	dir = opendir(proc_path);
-	if (dir == NULL)
-		return;
-
-	while ((f = readdir(dir)) != NULL) {
-		if (strcmp(f->d_name, "cmdline"))
-			continue;
-		
-		cat_path(tmp, pid_path, f->d_name);
-		FILE *fptr = fopen(tmp, "r");
-		while (fgets(tmp, LEN, fptr) != NULL) {
-			printf("%s\n", tmp);
-		}
-	}
+void get_pid(const char *path, Stat &stat) {
+	int l = strlen(path);
+	int index = l;
+	for (--index; path[index] != '/'; index--);
+	index++;
+	for (int i = 0; index < l; index++, i++)
+		stat.pid[i] = path[index];
+	//printf("pid = %s\n", stat.pid);
 }
 
-void traverse_proc() {
+void traverse_pid(const char *path, std::map<std::string, Stat> &table) {
+    DIR *dir;
+    struct dirent *f;
+    char fd_path[LEN] = {0};
+	char descriptor_path[LEN] = {0};
+	char inode[N] = {0};
+	char buf[LEN];
+	int l;
+	int indexI, indexJ;
+
+	cat_path(fd_path, path, "fd");
+    dir = opendir(fd_path);
+    if (dir == NULL)
+        return;
+
+    while ((f = readdir(dir)) != NULL) {
+		if (is_dot(f->d_name))
+			continue;
+
+		cat_path(descriptor_path, fd_path, f->d_name);
+		memset(buf, 0, LEN);
+		if (readlink(descriptor_path, buf, LEN) == -1)
+			printf("Unable to read link.\n");
+
+		l = strlen(buf);
+		if (buf[0] == '/' || buf[l-1] != ']' || buf[l-2] < '0' || buf[l-2] > '9' )
+			continue;
+
+		indexI = 0;
+		indexJ = 0;
+		while (buf[indexI++] != '[');
+		while (buf[indexI] != ']')
+			inode[indexJ++] = buf[indexI++];
+
+		if (table.find(inode) != table.end()) {
+			get_pid(path, table[inode]);
+			//printf("path = %s, inode = %s\n", path, inode);
+		}
+    }
+	closedir(dir);
+}
+
+void traverse_proc(std::map<std::string, Stat> &table) {
 	DIR *dir;
 	struct dirent *f;
-	char tmp[LEN] = {0};
+	char pid_path[LEN] = {0};
 
 	dir = opendir(proc_path);
 	if (dir == NULL)
 		return;
 
 	while ((f = readdir(dir)) != NULL) {
-		if (!strcmp(f->d_name, ".") || !strcmp(f->d_name, "..") || is_contain_letter(f->d_name))
+		if (is_dot(f->d_name) || is_not_pid(f->d_name))
 			continue;
 
-		printf("%s\n", f->d_name);
-		cat_path(tmp, proc_path, f->d_name);
-		traverse_pid(tmp);
+		cat_path(pid_path, proc_path, f->d_name);
+		traverse_pid(pid_path, table);
 	}
+	closedir(dir);
+}
+
+/*
+ * Get the information in /proc/net/tcp, tcp6, udp, udp6.
+ */
+void get_stat(std::vector<Stat> &base, const char *path, const char *protocol, std::map<std::string, Stat> &table) {
+	FILE *file_ptr = fopen(path, "r");
+	char buf[N];
+	int limit;
+
+    if (file_ptr == NULL) {
+        fprintf(stderr, "Unable to open %s\n", tcp_file_path);
+        exit(-1);
+    }
+	
+	/*
+	 * Read the labels.
+	 */
+	while(fgetc(file_ptr) != '\n');
+	
+	/*
+	 * Tcp and tcp6 have 17 columns, udp and udp6 have 13 columns.
+	 * 'limit' is for read the redundant columns.
+	 */
+	if (!strcmp(protocol, "tcp") || !strcmp(protocol, "tcp6"))
+		limit = 8;
+	else
+		limit = 4;
+
+	/*
+	 * Local address is at 2, foreign address is at 3 and inode is at 10.
+	 */
+    while (fscanf(file_ptr, "%s", buf) != -1) {
+		Stat tmp;
+	    char inode[N];
+
+		fscanf(file_ptr, "%s", tmp.local_address);
+		fscanf(file_ptr, "%s", tmp.foreign_address);
+		for (int i = 1; i < 7; i++)
+        	fscanf(file_ptr, "%s", buf);
+		fscanf(file_ptr, "%s", inode);
+		table.emplace(inode, tmp);
+		for (int i = 1; i < limit; i++)
+        	fscanf(file_ptr, "%s", buf);
+		
+		//printf("inode = %s", inode);
+		//printf(", local = %s", table[inode].local_address);
+		//printf(", foreign = %s\n", table[inode].foreign_address);
+    }
+	fclose(file_ptr);
 }
 
 int main(int argc, char **argv) {
-	/*
-	FILE *tcp_file = fopen(tcp_file_path, "r");
-	FILE *udp_file = fopen(udp_file_path, "r");
+	std::vector<Stat> tcp;
+	std::vector<Stat> tcp6;
+	std::vector<Stat> udp;
+	std::vector<Stat> udp6;
+	std::map<std::string, Stat> inode_table;
 
-	if (tcp_file_path == NULL) {
-		printf("The path of the tcp file is wrong.\n");
-		exit(-1);
-	}
-	if (udp_file_path == NULL) {
-		printf("The path of the udp file is wrong.\n");
-		exit(-1);
-	}
+	//printf("========== tcp ==========\n");
+	get_stat(tcp, tcp_file_path, "tcp", inode_table);
+	//printf("========== tcp6 ==========\n");
+	get_stat(tcp6, tcp6_file_path, "tcp6", inode_table);
+	//printf("========== udp ==========\n");
+	get_stat(udp, udp_file_path, "udp", inode_table);
+	//printf("========== udp6 ==========\n");
+	get_stat(udp6, udp6_file_path, "udp6", inode_table);
 
-	char tmp[LEN] = {0};
+	traverse_proc(inode_table);
 
-	while (fgets(tmp, LEN, tcp_file) != NULL) {
-		printf("%s", tmp);
-	}
-	puts("");
-	while (fgets(tmp, LEN, udp_file) != NULL) {
-		printf("%s", tmp);
-	}
-	*/
-
-	traverse_proc();
-	
 	return 0;
 }
