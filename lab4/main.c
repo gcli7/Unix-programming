e6ca1548ccd14f18388a7f60b520215ab5078158
gcli <star106121@gmail.com>
2019-06-24 15:45:56 +0800

    Revise "run" command.

--- lab4/main.c
+++ lab4/main.c
@@ -58,6 +58,81 @@ void elf_parse(const char *file_name) {
     }
 }
 
+int start_program(const char *file_name) {
+    child = fork();
+    if (child < 0)
+        print_error("fork failed!");
+    if (!child) {
+        if (ptrace(PTRACE_TRACEME, 0, 0, 0) < 0)
+            print_error("PTRACE_TRACEME failed!");
+        if (execlp(file_name, file_name, NULL) < 0)
+            print_error("execlp failed!");
+        exit(0);
+    }
+    else {
+        if (waitpid(child, &pid_status, 0) < 0)
+            print_error("waitpid failed!");
+        if (ptrace(PTRACE_SETOPTIONS, child, 0, PTRACE_O_EXITKILL) < 0 )
+            print_error("PTRACE_SETOPTIONS failed!");
+        printf("** pid %d\n", child);
+        status = running;
+    }
+    return 0;
+}
+
+int run_single() {
+    if(ptrace(PTRACE_SINGLESTEP, child, 0, 0) < 0)
+        print_error("PTRACE_SINGLESTEP failed!");
+    if (waitpid(child, &pid_status, 0) < 0)
+        print_error("waitpid failed!");
+    return 0;
+}
+
+int run_program() {
+    if (status == running)
+        printf("** program sample/hello64 is already running.\n");
+    else
+        start_program(program);
+    if (ptrace(PTRACE_CONT, child, 0, 0) < 0)
+        print_error("PTRACE_CONT failed!");
+    if (waitpid(child, &pid_status, 0) < 0)
+        print_error("waitpid failed!");
+    return 0;
+}
+
+int load_program(const char *file_name) {
+    if (access(file_name, 0)) {
+        printf("** The executable does not exist.\n");
+        return -1;
+    }
+
+    elf_parse(file_name);
+    strcpy(program, file_name);
+    status = loaded;
+
+    return 0;
+}
+
+int show_help() {
+	printf("** - break {instruction-address}: add a break point\n");
+	printf("** - cont: continue execution\n");
+	printf("** - delete {break-point-id}: remove a break point\n");
+	printf("** - disasm addr: disassemble instructions in a file or a memory region\n");
+	printf("** - dump addr [length]: dump memory content\n");
+	printf("** - exit: terminate the debugger\n");
+	printf("** - get reg: get a single value from a register\n");
+	printf("** - getregs: show registers\n");
+	printf("** - help: show this message\n");
+	printf("** - list: list break points\n");
+	printf("** - load {path/to/a/program}: load a program\n");
+	printf("** - run: run the program\n");
+	printf("** - vmmap: show memory layout\n");
+	printf("** - set reg val: get a single value to a register\n");
+	printf("** - si: step into instruction\n");
+	printf("** - start: start the program and stop at the first instruction\n");
+    return -1;
+}
+
 int get_all_registers() {
     struct user_regs_struct regs;
     if(ptrace(PTRACE_GETREGS, child, 0, &regs) < 0)
@@ -136,93 +211,21 @@ int get_register(const char *reg_name) {
     return 0;
 }
 
-int run_single() {
-    if(ptrace(PTRACE_SINGLESTEP, child, 0, 0) < 0)
-        print_error("PTRACE_SINGLESTEP failed!");
-    if (waitpid(child, &pid_status, 0) < 0)
-        print_error("waitpid failed!");
-    return 0;
-}
-
-int run_program() {
-    if (ptrace(PTRACE_CONT, child, 0, 0) < 0)
-        print_error("PTRACE_CONT failed!");
-    printf("** program sample/hello64 is already running.\n");
-    if (waitpid(child, &pid_status, 0) < 0)
-        print_error("waitpid failed!");
-    return 0;
-}
-
-int start_program(const char *file_name) {
-    child = fork();
-    if (child < 0)
-        print_error("fork failed!");
-    if (!child) {
-        if (ptrace(PTRACE_TRACEME, 0, 0, 0) < 0)
-            print_error("PTRACE_TRACEME failed!");
-        if (execlp(file_name, file_name, NULL) < 0)
-            print_error("execlp failed!");
-        exit(0);
-    }
-    else {
-        if (waitpid(child, &pid_status, 0) < 0)
-            print_error("waitpid failed!");
-        if (ptrace(PTRACE_SETOPTIONS, child, 0, PTRACE_O_EXITKILL) < 0 )
-            print_error("PTRACE_SETOPTIONS failed!");
-        printf("** pid %d\n", child);
-        status = running;
-    }
-    return 0;
-}
-
-int load_program(const char *file_name) {
-    if (access(file_name, 0)) {
-        printf("** The executable does not exist.\n");
-        return -1;
-    }
-
-    elf_parse(file_name);
-    strcpy(program, file_name);
-    status = loaded;
-
-    return 0;
-}
-
-int show_help() {
-	printf("** - break {instruction-address}: add a break point\n");
-	printf("** - cont: continue execution\n");
-	printf("** - delete {break-point-id}: remove a break point\n");
-	printf("** - disasm addr: disassemble instructions in a file or a memory region\n");
-	printf("** - dump addr [length]: dump memory content\n");
-	printf("** - exit: terminate the debugger\n");
-	printf("** - get reg: get a single value from a register\n");
-	printf("** - getregs: show registers\n");
-	printf("** - help: show this message\n");
-	printf("** - list: list break points\n");
-	printf("** - load {path/to/a/program}: load a program\n");
-	printf("** - run: run the program\n");
-	printf("** - vmmap: show memory layout\n");
-	printf("** - set reg val: get a single value to a register\n");
-	printf("** - si: step into instruction\n");
-	printf("** - start: start the program and stop at the first instruction\n");
-    return -1;
-}
-
 int command() {
-    if (!strcmp(input[0], "load") && status == non_loaded)
-        return load_program(input[1]);
-    else if (!strcmp(input[0], "start") && status == loaded)
-        return start_program(program);
-    else if ((!strcmp(input[0], "r") || !strcmp(input[0], "run")) && status == running)
-        return run_program();
-    else if (!strcmp(input[0], "si") && status == running)
-        return run_single();
-    else if ((!strcmp(input[0], "g") || !strcmp(input[0], "get")) && status == running)
+    if ((!strcmp(input[0], "g") || !strcmp(input[0], "get")) && status == running)
         return get_register(input[1]);
     else if (!strcmp(input[0], "getregs") && status == running)
         return get_all_registers();
     else if (!strcmp(input[0], "help"))
         return show_help();
+    else if (!strcmp(input[0], "load") && status == non_loaded)
+        return load_program(input[1]);
+    else if ((!strcmp(input[0], "r") || !strcmp(input[0], "run")) && (status == loaded || status == running))
+        return run_program();
+    else if (!strcmp(input[0], "si") && status == running)
+        return run_single();
+    else if (!strcmp(input[0], "start") && status == loaded)
+        return start_program(program);
 
     ILLEGAL;
     return -1;
@@ -256,7 +259,7 @@ int main(int argc, char **argv) {
         if (wait_input())
             continue;
         if (!WIFSTOPPED(pid_status))
-            break;
+            status = loaded;
     }
 
     return 0;
